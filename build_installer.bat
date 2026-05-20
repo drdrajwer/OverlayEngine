4d420f85c26729a80192e1b66251d9aeb46eedba
drdrajwer <drdrajwer@gmail.com>
2026-05-20 20:57:47 +0200

    Use vpk pack (no windows subcommand in 0.0.1298)

--- build_installer.bat
+++ build_installer.bat
@@ -74,7 +74,7 @@ if exist "%RELEASES_DIR%" (
     rmdir /s /q "%RELEASES_DIR%"
 )
 
-vpk windows pack ^
+vpk pack ^
     --packId "%APP_ID%" ^
     --packVersion "%APP_VERSION%" ^
     --packDir "%PUBLISH_DIR%" ^
